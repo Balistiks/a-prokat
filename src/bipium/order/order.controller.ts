11b20d44946e58dade6644d885f9ba176f34bbab
balistik <balistiktw@gmail.com>
2023-11-22 02:15:12 +1000

    make webhook for update order

--- src/bipium/order/order.controller.ts
+++ src/bipium/order/order.controller.ts
@@ -1,8 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Request } from "express";
 
 // Контроллер для принятия запросов по заявкам
 @Controller('bipium/order')
 export class BipiumOrderController {
-  constructor() {
+  constructor() {}
+
+  @Post('update')
+  async update(
+    @Req() request: Request,
+    @Body() body
+  ) {
+    console.log(body)
   }
 }
