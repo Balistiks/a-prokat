a25a94bf4f69eec0ff94681ca6ce204a23cb650e
balistik <balistiktw@gmail.com>
2023-11-24 11:50:57 +1000

    make baseURL with env.HOST

--- front/src/components/form-create-order/FormCreateOrder.vue
+++ front/src/components/form-create-order/FormCreateOrder.vue
@@ -10,7 +10,7 @@
 <script>
 import axios from 'axios';
 const http = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://${import.meta.env.HOST}:3000`,
   headers: {
     'Content-Type': 'application/json',
   },
